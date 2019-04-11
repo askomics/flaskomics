7120881849e36168a3d85ac10ce5be340d767e5b
xgaia <xgaia@gmx.com>
2019-04-11 11:53:58 +0200

    abstraction test

--- tests/test_api_startpoints.py
+++ tests/test_api_startpoints.py
@@ -7,10 +7,8 @@ class TestApiStartpoints(AskomicsTestCase):
     def test_startpoints(self, client_logged_as_jdoe_with_data):
         """Test /api/startpoints route"""
         gene_timestamp = client_logged_as_jdoe_with_data.gene_timestamp
-        print(gene_timestamp)
         response = client_logged_as_jdoe_with_data.get('/api/startpoints')
         assert response.status_code == 200
-        print(response.json)
         assert response.json == {
             'error': False,
             'errorMessage': '',
@@ -27,4 +25,76 @@ class TestApiStartpoints(AskomicsTestCase):
                 'private': True,
                 'public': False
             }]
+        }
+
+    def test_get_abstraction(self, client_logged_as_jdoe_with_data):
+        """Test /api/startpoints/abstraction route"""
+        gene_timestamp = client_logged_as_jdoe_with_data.gene_timestamp
+        response = client_logged_as_jdoe_with_data.get('/api/startpoints/abstraction')
+        assert response.status_code == 200
+        print(response.json)
+        assert response.json == {
+            'abstraction': [{
+                'attributes':
+                [{
+                    'label': 'end',
+                    'type': 'http://www.w3.org/2001/XMLSchema#decimal',
+                    'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#end'
+                }, {
+                    'label': 'start',
+                    'type': 'http://www.w3.org/2001/XMLSchema#decimal',
+                    'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#start'
+                }, {
+                    'label': 'chromosome',
+                    'type': 'http://www.semanticweb.org/user/ontologies/2018/1#AskomicsCategory',
+                    'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#chromosome',
+                    'values':
+                    [{
+                        'label': 'AT1',
+                        'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#AT1'
+                    }, {
+                        'label': 'AT2',
+                        'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#AT2'
+                    }, {
+                        'label': 'AT3',
+                        'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#AT3'
+                    }, {
+                        'label': 'BN1',
+                        'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#BN1'
+                    }, {
+                        'label': 'BN2',
+                        'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#BN2'
+                    }]
+                }, {
+                    'label': 'organism',
+                    'type': 'http://www.semanticweb.org/user/ontologies/2018/1#AskomicsCategory',
+                    'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#organism',
+                    'values':
+                        [{
+                            'label': 'Arabidopsis thaliana',
+                            'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#Arabidopsis%20thaliana'
+                        }, {
+                            'label': 'Brassica napus',
+                            'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#Brassica%20napus'
+                        }]
+                }, {
+                    'label': 'strand',
+                    'type': 'http://www.semanticweb.org/user/ontologies/2018/1#AskomicsCategory',
+                    'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#strand',
+                    'values':
+                        [{
+                            'label': 'plus',
+                            'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#plus'
+                        }, {
+                            'label': 'minus',
+                            'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#minus'
+                        }]
+                }],
+                'graphs': ['urn:sparql:askomics_test:1_jdoe:gene_{}'.format(gene_timestamp)],
+                'label': 'Gene',
+                'relations': [],
+                'uri': 'http://www.semanticweb.org/user/ontologies/2018/1#Gene'
+            }],
+            'error': False,
+            'errorMessage': ''
         }
