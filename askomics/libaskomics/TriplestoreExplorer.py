1ad9345d87c1584420b80819e443b7b9bab6a4c4
xgaia <xgaia@gmx.com>
2019-09-12 14:57:03 +0200

    divide get_abstraction into 3 smaller queries

--- askomics/libaskomics/TriplestoreExplorer.py
+++ askomics/libaskomics/TriplestoreExplorer.py
@@ -87,12 +87,26 @@ class TriplestoreExplorer(Params):
         return startpoints
 
     def get_abstraction(self):
-        """Get user abstraction from the triplestore
+        """Get AskOmics Abstraction
+
+        Returns
+        -------
+        dict
+            AskOmics abstraction
+        """
+        return {
+            "entities": self.get_abstraction_entities(),
+            "attributes": self.get_abstraction_attributes(),
+            "relations": self.get_abstraction_relations()
+        }
+
+    def get_abstraction_entities(self):
+        """Get abstraction entities
 
         Returns
         -------
         list
-            AskOmics abstraction
+            List of entities available
         """
         filter_user = ""
         if self.logged_user():
@@ -102,53 +116,20 @@ class TriplestoreExplorer(Params):
         query_builder = SparqlQueryBuilder(self.app, self.session)
 
         query = '''
-        SELECT DISTINCT ?graph ?entity_uri ?entity_faldo ?entity_label ?node_type ?attribute_uri ?attribute_faldo ?attribute_label ?attribute_range ?property_uri ?property_faldo ?property_type ?property_label ?range_uri ?category_value_uri ?category_value_label
+        SELECT DISTINCT ?graph ?entity_uri ?entity_type ?entity_faldo ?entity_label
         WHERE {{
-            # Graphs
             ?graph :public ?public .
             ?graph dc:creator ?creator .
             GRAPH ?graph {{
-                # Entities
-                ?entity_uri a ?node_type .
-                VALUES ?node_type {{ :entity :bnode }} .
+                ?entity_uri a ?entity_type .
+                VALUES ?entity_type {{ :entity :bnode }} .
                 # Faldo
                 OPTIONAL {{
                     ?entity_uri a ?entity_faldo .
-                    VALUES ?entity_faldo {{ :faldo }}
+                    VALUES ?entity_faldo {{ :faldo }} .
                 }}
-                OPTIONAL {{
-                    ?entity_uri rdfs:label ?entity_label .
-                }}
-                # Attributes
-                OPTIONAL {{
-                    ?attribute_uri a owl:DatatypeProperty .
-                    ?attribute_uri rdfs:label ?attribute_label .
-                    ?attribute_uri rdfs:domain ?entity_uri .
-                    ?attribute_uri rdfs:range ?attribute_range .
-                    # Faldo
-                    OPTIONAL {{
-                        ?attribute_uri a ?attribute_faldo .
-                        VALUES ?attribute_faldo {{ askomics:faldoStart askomics:faldoEnd }}
-                    }}
-                }}
-                # Property (relations and categories)
-                OPTIONAL {{
-                    ?property_uri a owl:ObjectProperty .
-                    ?property_uri rdfs:label ?property_label .
-                    ?property_uri rdfs:domain ?entity_uri .
-                    ?property_uri rdfs:range ?range_uri .
-                    ?property_uri a ?property_type .
-                    # Faldo
-                    OPTIONAL {{
-                        ?property_uri a ?property_faldo .
-                        VALUES ?property_faldo {{ askomics:faldoStrand askomics:faldoReference }}
-                    }}
-                }}
-                # Categories (DK)
-                OPTIONAL {{
-                    ?range_uri askomics:category ?category_value_uri .
-                    ?category_value_uri rdfs:label ?category_value_label .
-                }}
+                # Label
+                OPTIONAL {{ ?entity_uri rdfs:label ?entity_label . }}
             }}
             FILTER (
                 ?public = <true>{}
@@ -159,14 +140,7 @@ class TriplestoreExplorer(Params):
         header, data = query_launcher.process_query(query_builder.prefix_query(query))
 
         entities_list = []  # list of entity uri
-        attributes_list = []
-        relations_list = []
-
         entities = []  # list of entity dict
-        attributes = []
-        relations = []
-
-        abstraction = []
 
         for result in data:
             if result["entity_uri"] not in entities_list:
@@ -174,10 +148,10 @@ class TriplestoreExplorer(Params):
                 entities_list.append(result["entity_uri"])
                 # Uri, graph and label
                 label = "" if "entity_label" not in result else result["entity_label"]
-                node_type = "bnode" if result["node_type"] == "{}bnode".format(self.settings.get("triplestore", "prefix")) else "node"
+                entity_type = "bnode" if result["entity_type"] == "{}bnode".format(self.settings.get("triplestore", "prefix")) else "node"
                 entity = {
                     "uri": result["entity_uri"],
-                    "type": node_type,
+                    "type": entity_type,
                     "label": label,
                     "faldo": True if "entity_faldo" in result else False,
                     "graphs": [result["graph"]],
@@ -190,11 +164,60 @@ class TriplestoreExplorer(Params):
                 if result["graph"] not in entities[index_entity]["graphs"]:
                     entities[index_entity]["graphs"].append(result["graph"])
 
-            # Get index of the current entity
-            index_entity = entities_list.index(result['entity_uri'])
+        return entities
 
+    def get_abstraction_attributes(self):
+        """Get user abstraction attributes from the triplestore
+
+        Returns
+        -------
+        list
+            AskOmics attributes
+        """
+        filter_user = ""
+        if self.logged_user():
+            filter_user = " || ?creator = <{}>".format(self.session["user"]["username"])
+
+        query_launcher = SparqlQueryLauncher(self.app, self.session)
+        query_builder = SparqlQueryBuilder(self.app, self.session)
+
+        query = '''
+        SELECT DISTINCT ?graph ?entity_uri ?attribute_uri ?attribute_type ?attribute_faldo ?attribute_label ?attribute_range ?category_value_uri ?category_value_label
+        WHERE {{
+            # Graphs
+            ?graph :public ?public .
+            ?graph dc:creator ?creator .
+            GRAPH ?graph {{
+                ?attribute_uri a ?attribute_type .
+                VALUES ?attribute_type {{ owl:DatatypeProperty :AskomicsCategory }}
+                ?attribute_uri rdfs:label ?attribute_label .
+                ?attribute_uri rdfs:domain ?entity_uri .
+                ?attribute_uri rdfs:range ?attribute_range .
+                # Faldo
+                OPTIONAL {{
+                    ?attribute_uri a ?attribute_faldo .
+                    VALUES ?attribute_faldo {{ askomics:faldoStart askomics:faldoEnd askomics:faldoStrand askomics:faldoReference }}
+                }}
+                # Categories (DK)
+                OPTIONAL {{
+                    ?attribute_range askomics:category ?category_value_uri .
+                    ?category_value_uri rdfs:label ?category_value_label .
+                }}
+            }}
+            FILTER (
+                ?public = <true>{}
+            )
+        }}
+        '''.format(filter_user)
+
+        header, data = query_launcher.process_query(query_builder.prefix_query(query))
+        attributes_list = []
+
+        attributes = []
+
+        for result in data:
             # Attributes
-            if "attribute_uri" in result and "attribute_label" in result:
+            if "attribute_uri" in result and "attribute_label" in result and result["attribute_type"] != "{}AskomicsCategory".format(self.settings.get("triplestore", "prefix")):
                 attr_tpl = (result["attribute_uri"], result["entity_uri"])
                 if attr_tpl not in attributes_list:
                     attributes_list.append(attr_tpl)
@@ -204,7 +227,8 @@ class TriplestoreExplorer(Params):
                         "graphs": [result["graph"], ],
                         "entityUri": result["entity_uri"],
                         "type": result["attribute_range"],
-                        "faldo": result["attribute_faldo"] if "attribute_faldo" in result else None
+                        "faldo": result["attribute_faldo"] if "attribute_faldo" in result else None,
+                        "categories": []
                     }
                     attributes.append(attribute)
                 else:
@@ -216,17 +240,17 @@ class TriplestoreExplorer(Params):
                 index_attribute = attributes_list.index(attr_tpl)
 
             # Categories
-            if "property_uri" in result and result["property_type"] == "{}AskomicsCategory".format(self.settings.get("triplestore", "prefix")):
-                attr_tpl = (result["property_uri"], result["entity_uri"])
+            if "attribute_uri" in result and result["attribute_type"] == "{}AskomicsCategory".format(self.settings.get("triplestore", "prefix")):
+                attr_tpl = (result["attribute_uri"], result["entity_uri"])
                 if attr_tpl not in attributes_list:
                     attributes_list.append(attr_tpl)
                     attribute = {
-                        "uri": result["property_uri"],
-                        "label": result["property_label"],
+                        "uri": result["attribute_uri"],
+                        "label": result["attribute_label"],
                         "graphs": [result["graph"], ],
                         "entityUri": result["entity_uri"],
-                        "type": result["property_type"],
-                        "faldo": result["property_faldo"] if "property_faldo" in result else None,
+                        "type": result["attribute_type"],
+                        "faldo": result["attribute_faldo"] if "attribute_faldo" in result else None,
                         "categories": [{
                             "uri": result["category_value_uri"],
                             "label": result["category_value_label"]
@@ -246,6 +270,49 @@ class TriplestoreExplorer(Params):
                     if value not in attributes[index_attribute]["categories"]:
                         attributes[index_attribute]["categories"].append(value)
 
+        return attributes
+
+    def get_abstraction_relations(self):
+        """Get user abstraction relations from the triplestore
+
+        Returns
+        -------
+        list
+            Relations
+        """
+        filter_user = ""
+        if self.logged_user():
+            filter_user = " || ?creator = <{}>".format(self.session["user"]["username"])
+
+        query_launcher = SparqlQueryLauncher(self.app, self.session)
+        query_builder = SparqlQueryBuilder(self.app, self.session)
+
+        query = '''
+        SELECT DISTINCT ?graph ?entity_uri ?entity_faldo ?entity_label ?node_type ?attribute_uri ?attribute_faldo ?attribute_label ?attribute_range ?property_uri ?property_faldo ?property_type ?property_label ?range_uri ?category_value_uri ?category_value_label
+        WHERE {{
+            # Graphs
+            ?graph :public ?public .
+            ?graph dc:creator ?creator .
+            GRAPH ?graph {{
+                # Property (relations and categories)
+                ?property_uri a owl:ObjectProperty .
+                ?property_uri rdfs:label ?property_label .
+                ?property_uri rdfs:domain ?entity_uri .
+                ?property_uri rdfs:range ?range_uri .
+                ?property_uri a ?property_type .
+            }}
+            FILTER (
+                ?public = <true>{}
+            )
+        }}
+        '''.format(filter_user)
+
+        header, data = query_launcher.process_query(query_builder.prefix_query(query))
+
+        relations_list = []
+        relations = []
+
+        for result in data:
             # Relation
             if "property_uri" in result and result["property_type"] == "{}AskomicsRelation".format(self.settings.get("triplestore", "prefix")):
                 rel_tpl = (result["property_uri"], result["entity_uri"], result["range_uri"])
@@ -265,13 +332,7 @@ class TriplestoreExplorer(Params):
                     if result["graph"] not in relations[index_relation]["graphs"]:
                         relations[index_relation]["graphs"].append(result["graph"])
 
-        abstraction = {
-            "entities": entities,
-            "attributes": attributes,
-            "relations": relations
-        }
-
-        return abstraction
+        return relations
 
     def get_attribute_index(self, uri, attribute_list):
         """Get attribute index
