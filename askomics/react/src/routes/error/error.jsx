5351d12eeb4b75669d3c9bd0c13ae50b81f01d47
xgaia <xgaia@gmx.com>
2019-10-15 11:54:06 +0200

    set custom error messages

--- askomics/react/src/routes/error/error.jsx
+++ askomics/react/src/routes/error/error.jsx
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import { Alert } from 'reactstrap'
 import { Redirect } from 'react-router'
 import PropTypes from 'prop-types'
+import Utils from '../../classes/utils'
 
 export default class ErrorDiv extends Component {
   constructor (props) {
     super(props)
+    this.utils = new Utils()
   }
 
   render () {
@@ -17,10 +19,16 @@ export default class ErrorDiv extends Component {
       return null
     }
 
+    let messages = {
+      "404": this.utils.objectHaveKeys(this.props, "customMessages", "404") ? this.props.customMessages["404"] : "404 Not Found",
+      "503": this.utils.objectHaveKeys(this.props, "customMessages", "503") ? this.props.customMessages["503"] : "503 Service Unavailable ",
+      "504": this.utils.objectHaveKeys(this.props, "customMessages", "504") ? this.props.customMessages["504"] : "504 Gateway Time-out",
+      "500": this.utils.objectHaveKeys(this.props, "customMessages", "500") ? this.props.customMessages["500"] : this.props.errorMessage ? this.props.errorMessage : "500 Internal Server Error",
+    }
 
     let errorMessage = this.props.errorMessage
-    if (this.props.status == 404) {
-      errorMessage = "404 not found"
+    if (this.utils.objectHaveKeys(this.props, "status")) {
+      errorMessage = messages[this.props.status.toString()]
     }
 
     let error
