2457892f39f05d3d55a73eb43ccbc04b5565321d
xgaia <xgaia@gmx.com>
2019-11-06 11:23:21 +0100

    test set_quota

--- tests/test_api_admin.py
+++ tests/test_api_admin.py
@@ -55,6 +55,47 @@ class TestApiAdmin(AskomicsTestCase):
         assert response.status_code == 200
         assert response.json == {'error': False, 'errorMessage': ''}
 
+    def test_setquota(self, client):
+        """test the /api/admin/setadmin route"""
+        client.create_two_users()
+        client.log_user("jdoe")
+        client.upload()
+
+        set_quota = {
+            "username": "jsmith",
+            "quota": "10mb"
+        }
+
+        response = client.client.post('/api/admin/setquota', json=set_quota)
+        expected = {
+            'error': False,
+            'errorMessage': '',
+            'users': [{
+                'admin': 1,
+                'blocked': 0,
+                'email': 'jdoe@askomics.org',
+                'fname': 'John',
+                'galaxy': None,
+                'ldap': 0,
+                'lname': 'Doe',
+                'quota': 0,
+                'username': 'jdoe'
+            }, {
+                'admin': 0,
+                'blocked': 0,
+                'email': 'jsmith@askomics.org',
+                'fname': 'Jane',
+                'galaxy': None,
+                'ldap': 0,
+                'lname': 'Smith',
+                'quota': 10000000,
+                'username': 'jsmith'
+            }]
+        }
+
+        assert response.status_code == 200
+        assert response.json == expected
+
     def test_setblocked(self, client):
         """test the /api/admin/setblocked route"""
         client.create_two_users()
