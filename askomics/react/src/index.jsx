b6a5f364d2474a7f9e2279508d0eb0a65260bfc7
xgaia <xgaia@gmx.com>
2019-11-08 15:19:22 +0100

    sentry: celery

--- askomics/react/src/index.jsx
+++ askomics/react/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Sentry from '@sentry/browser'
+import packageJson from '../../../package.json';
 
 import Routes from './routes'
 
@@ -15,7 +16,10 @@ class App extends React.Component {
 
 let sentry = document.getElementById('sentry').getAttribute('sentry')
 if (sentry != "") {
-  Sentry.init({dsn: sentry});
+  Sentry.init({
+    dsn: sentry,
+    release: packageJson.name + "@" + packageJson.version
+  });
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'))
