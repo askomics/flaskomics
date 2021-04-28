88daf8ad23fba1a4fa0ffda184f77df7ebe0d6b3
mboudet <mateo.boudet@gmail.com>
2021-04-28 07:49:54 +0000

    Fix ordering

--- askomics/react/src/routes/sparql/resultstable.jsx
+++ askomics/react/src/routes/sparql/resultstable.jsx
@@ -9,11 +9,37 @@ import Utils from '../../classes/utils'
 export default class ResultsTable extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+        filter_columns: {}
+    }
     this.utils = new Utils()
+    this.custom_compare = this.custom_compare.bind(this)
+  }
+
+  custom_compare(a, b, column_name){
+    let result, num_a, num_b;
+    if (typeof b === 'string') {
+      if (this.state.filter_columns[column_name]){
+        num_a = Number(a)
+        num_b = Number(b)
+        if (Number.isNaN(num_a) || Number.isNaN(num_b)){
+          this.state.filter_columns[column_name] = false
+          result = b.localeCompare(a);
+        } else {
+          result = num_a > num_b ? -1 : ((num_a < num_b) ? 1 : 0);
+        }
+      } else {
+        result = b.localeCompare(a);
+      }
+    } else {
+      result = a > b ? -1 : ((a < b) ? 1 : 0);
+    }
+    return result;
   }
 
   render () {
     let columns = this.props.header.map((colName, index) => {
+      this.state.filter_columns[colName] = true;
       return ({
         dataField: colName,
         text: colName,
@@ -24,6 +50,12 @@ export default class ResultsTable extends Component {
             return <a href={cell}>{this.utils.splitUrl(cell)}</a>
           }
           return cell
+        },
+        sortFunc: (a, b, order, dataField, rowA, rowB) => {
+          if (order === 'asc') {
+            return this.custom_compare(a,b, dataField);
+          }
+          return this.custom_compare(b,a, dataField); // desc
         }
       })
     })
