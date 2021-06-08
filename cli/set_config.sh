077b3e0fdea1fda1fd326e58fa84558904e62356
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2021-06-08 18:03:47 +0200

    Fix #225: edit config file in temp file rather than in final destination

--- cli/set_config.sh
+++ cli/set_config.sh
@@ -3,10 +3,10 @@
 config_template_path="config/askomics.ini.template"
 config_path="config/askomics.ini"
 
-# Create config file
-if [[ ! -f $config_path ]]; then
-    cp $config_template_path $config_path
-fi
+tmpfile=$(mktemp /tmp/askomics.ini.XXXXXX)
+
+# Init config file
+cp $config_template_path $tmpfile
 
 # Convert env to ini entry
 printenv | egrep "ASKO_" | while read setting
@@ -14,7 +14,9 @@ do
     section=$(echo $setting | egrep -o "^ASKO[^=]+" | sed 's/^.\{5\}//g' | cut -d "_" -f 1)
     key=$(echo $setting | egrep -o "^ASKO[^=]+" | sed 's/^.\{5\}//g' | sed "s/$section\_//g")
     value=$(echo $setting | egrep -o "=.*$" | sed 's/^=//g')
-    # crudini --set ${config_path} "${section}" "${key}" "${value}"
-    python3 cli/config_updater.py -p $config_path -s "${section}" -k "${key}" -v "${value}"
-    $cmd
+    # crudini --set ${tmpfile} "${section}" "${key}" "${value}"
+    python3 cli/config_updater.py -p $tmpfile -s "${section}" -k "${key}" -v "${value}"
 done
+
+# config ready, copy to dest
+cp $tmpfile $config_path
