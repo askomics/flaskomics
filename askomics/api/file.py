cd24f6a52ee914542d7d62411917255cf6ed2165
mboudet <mateo.boudet@gmail.com>
2021-08-12 08:58:22 +0000

    typo

--- askomics/api/file.py
+++ askomics/api/file.py
@@ -319,8 +319,8 @@ def integrate():
 
         for file in files_handler.files:
 
-            data["externalEndpoint"] = data["externalEndpoint"] if data.get("externalEndpoint") else None
-            data["customUri"] = data["customUri"] if (data.get("customUri") and isinstance(file, RdfFile)) else None
+            data["externalEndpoint"] = data["externalEndpoint"] if (data.get("externalEndpoint") and isinstance(file, RdfFile)) else None
+            data["customUri"] = data["customUri"] if data.get("customUri") else None
 
             dataset_info = {
                 "celery_id": None,
