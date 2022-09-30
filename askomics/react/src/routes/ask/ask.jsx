c22a2c541a8aad342ede442894a363f27fbced99
mboudet <mateo.boudet@gmail.com>
2022-09-30 14:56:53 +0000

    typo

--- askomics/react/src/routes/ask/ask.jsx
+++ askomics/react/src/routes/ask/ask.jsx
@@ -460,7 +460,7 @@ export default class Ask extends Component {
     let HtmlFrontMessage = (
       <div>
         <h2>Ask!</h2>
-        {this.state.config.frontMessage}
+        {this.props.config.frontMessage}
         <hr />
       </div>
     )
